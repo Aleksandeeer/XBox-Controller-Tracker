cd4220645885ecdaa013dd75babdf7099e0c6ed4
Александр <90092786+Aleksandeeer@users.noreply.github.com>
2022-12-25 13:00:29 +0300

    Add files via upload

---
+++ XBoxControllerApp/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace XBoxControllerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.RightProgressBar = new System.Windows.Forms.ProgressBar();
+            this.LeftProgressBar = new System.Windows.Forms.ProgressBar();
+            this.ChangeButton = new Krypton.Toolkit.KryptonButton();
+            this.BreakProgressBar = new XBoxControllerApp.VerticalProgressBar();
+            this.ThrottleProgressBar = new XBoxControllerApp.VerticalProgressBar();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // RightProgressBar
+            // 
+            this.RightProgressBar.Location = new System.Drawing.Point(132, 12);
+            this.RightProgressBar.Maximum = 32767;
+            this.RightProgressBar.Name = "RightProgressBar";
+            this.RightProgressBar.Size = new System.Drawing.Size(120, 20);
+            this.RightProgressBar.TabIndex = 1;
+            // 
+            // LeftProgressBar
+            // 
+            this.LeftProgressBar.Location = new System.Drawing.Point(12, 12);
+            this.LeftProgressBar.Maximum = 32768;
+            this.LeftProgressBar.Name = "LeftProgressBar";
+            this.LeftProgressBar.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.LeftProgressBar.RightToLeftLayout = true;
+            this.LeftProgressBar.Size = new System.Drawing.Size(120, 20);
+            this.LeftProgressBar.TabIndex = 2;
+            // 
+            // ChangeButton
+            // 
+            this.ChangeButton.CornerRoundingRadius = 10F;
+            this.ChangeButton.Location = new System.Drawing.Point(98, 137);
+            this.ChangeButton.Name = "ChangeButton";
+            this.ChangeButton.PaletteMode = Krypton.Toolkit.PaletteMode.ProfessionalSystem;
+            this.ChangeButton.Size = new System.Drawing.Size(68, 25);
+            this.ChangeButton.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ChangeButton.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ChangeButton.StateCommon.Border.DrawBorders = ((Krypton.Toolkit.PaletteDrawBorders)((((Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.ChangeButton.StateCommon.Border.Rounding = 10F;
+            this.ChangeButton.TabIndex = 5;
+            this.ChangeButton.Values.Text = "";
+            this.ChangeButton.Click += new System.EventHandler(this.ChangeButton_Click);
+            // 
+            // BreakProgressBar
+            // 
+            this.BreakProgressBar.BackColor = System.Drawing.Color.Red;
+            this.BreakProgressBar.ForeColor = System.Drawing.Color.Red;
+            this.BreakProgressBar.Location = new System.Drawing.Point(12, 38);
+            this.BreakProgressBar.Maximum = 255;
+            this.BreakProgressBar.Name = "BreakProgressBar";
+            this.BreakProgressBar.Size = new System.Drawing.Size(80, 124);
+            this.BreakProgressBar.TabIndex = 4;
+            this.BreakProgressBar.UseKrypton = true;
+            // 
+            // ThrottleProgressBar
+            // 
+            this.ThrottleProgressBar.BackColor = System.Drawing.Color.Lime;
+            this.ThrottleProgressBar.ForeColor = System.Drawing.Color.Lime;
+            this.ThrottleProgressBar.Location = new System.Drawing.Point(172, 38);
+            this.ThrottleProgressBar.Maximum = 255;
+            this.ThrottleProgressBar.Name = "ThrottleProgressBar";
+            this.ThrottleProgressBar.Size = new System.Drawing.Size(80, 124);
+            this.ThrottleProgressBar.TabIndex = 3;
+            this.ThrottleProgressBar.UseKrypton = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(266, 174);
+            this.Controls.Add(this.ChangeButton);
+            this.Controls.Add(this.BreakProgressBar);
+            this.Controls.Add(this.ThrottleProgressBar);
+            this.Controls.Add(this.LeftProgressBar);
+            this.Controls.Add(this.RightProgressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Геймпад";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ProgressBar RightProgressBar;
+        private System.Windows.Forms.ProgressBar LeftProgressBar;
+        private VerticalProgressBar BreakProgressBar;
+        private VerticalProgressBar ThrottleProgressBar;
+        private Krypton.Toolkit.KryptonButton ChangeButton;
+    }
+}
+
---
+++ XBoxControllerApp/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using SharpDX.XInput;
+
+namespace XBoxControllerApp
+{
+    public partial class Form1 : Krypton.Toolkit.KryptonForm
+    {
+        private Controller controller;
+        int xLeftStick;
+        byte throttleTrigger, breakTrigger;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(950, 102);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            controller = new Controller(UserIndex.One);
+
+            timer.Start();
+        }
+
+        private void ChangeButton_Click(object sender, EventArgs e)
+        {
+            if (FormBorderStyle == FormBorderStyle.FixedSingle)
+                FormBorderStyle = FormBorderStyle.None;
+            else
+                FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            controller.GetState(out State state);
+            //Левый стик по оси X - управление
+            xLeftStick = state.Gamepad.LeftThumbX;
+            //Правый стик - газ
+            throttleTrigger = state.Gamepad.RightTrigger;
+            //Левый стик - тормоз
+            breakTrigger = state.Gamepad.LeftTrigger;
+
+            //Установка значений - газ
+            ThrottleProgressBar.Value = throttleTrigger;
+            if (throttleTrigger > 0)
+                ThrottleProgressBar.Value = throttleTrigger - 1;
+
+            //Установка значений - тормоз
+            BreakProgressBar.Value = breakTrigger;
+            if (breakTrigger > 0)
+                BreakProgressBar.Value = breakTrigger - 1;
+
+            //Установка значений - управление
+            if (xLeftStick > 0)
+            {
+                RightProgressBar.Value = xLeftStick;
+                RightProgressBar.Value = xLeftStick - 1;
+                LeftProgressBar.Value = 0;
+            }
+            else
+            {
+
+                LeftProgressBar.Value = Math.Abs(xLeftStick);
+                LeftProgressBar.Value = Math.Abs(xLeftStick + 1);
+                RightProgressBar.Value = 0;
+            }
+        }
+    }
+
+    public class VerticalProgressBar : Krypton.Toolkit.KryptonProgressBar
+    {
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.Style |= 0x04;
+                return cp;
+            }
+        }
+    }
+}
